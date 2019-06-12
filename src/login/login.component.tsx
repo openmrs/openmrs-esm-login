f5eb31a1040b3d9a8a9ed51c266c345a3b9e325f
Joel Denning <joeldenning@gmail.com>
2019-06-12 09:07:13 +0300

    Redirecting to patient dashboard after successful login.

--- src/login/login.component.tsx
+++ src/login/login.component.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { css } from "@emotion/core";
 import { performLogin } from "./login.resource";
+import { Redirect } from "react-router-dom";
 
 export default function Login(props: LoginProps) {
   const [username, setUsername] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [isLoggingIn, setIsLoggingIn] = React.useState(false);
+  const [redirectToHomePage, setRedirectToHomePage] = React.useState(false);
 
   React.useEffect(() => {
     if (isLoggingIn) {
       performLogin(username, password)
         .then(data => {
-          alert("We got the user! " + JSON.stringify(data));
+          setRedirectToHomePage(true);
         })
         .finally(() => {
           setIsLoggingIn(false);
@@ -57,100 +59,104 @@ export default function Login(props: LoginProps) {
     border-radius: 4px;
   `;
 
-  return (
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100vw;
-        height: 100vh;
-        background: #fafbfc;
-      `}
-    >
+  if (redirectToHomePage) {
+    return <Redirect to="/patient-dashboard" />;
+  } else {
+    return (
       <div
         css={css`
-          padding: 16px;
-          border: 1px solid lightgray;
-          border-radius: 5px;
-          background: white;
-          width: 300px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100vw;
+          height: 100vh;
+          background: #fafbfc;
         `}
       >
         <div
           css={css`
-            margin: 20px;
+            padding: 16px;
+            border: 1px solid lightgray;
+            border-radius: 5px;
+            background: white;
+            width: 300px;
           `}
         >
-          <img
-            src={getLogoURL()}
-            alt="openmrs-logo"
+          <div
             css={css`
-              max-width: 85%;
-              max-height: 100%;
-              display: block;
-              margin: auto;
+              margin: 20px;
             `}
-          />
+          >
+            <img
+              src={getLogoURL()}
+              alt="openmrs-logo"
+              css={css`
+                max-width: 85%;
+                max-height: 100%;
+                display: block;
+                margin: auto;
+              `}
+            />
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            css={css`
+              margin: 10px;
+            `}
+          >
+            <div>
+              <label>
+                <input
+                  css={css`
+                    ${input}
+                  `}
+                  type="text"
+                  name="username"
+                  value={username}
+                  onChange={evt => setUsername(evt.target.value)}
+                  placeholder="Username"
+                  autoFocus
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                <input
+                  css={css`
+                    ${input}
+                  `}
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={evt => setPassword(evt.target.value)}
+                  placeholder="Password"
+                />
+              </label>
+            </div>
+            <div>
+              <small
+                css={css`
+                  color: #1a73e8;
+                `}
+              >
+                Forgot Password?
+              </small>
+            </div>
+            <div>
+              <button
+                css={css`
+                  ${btn}
+                `}
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-        <form
-          onSubmit={handleSubmit}
-          css={css`
-            margin: 10px;
-          `}
-        >
-          <div>
-            <label>
-              <input
-                css={css`
-                  ${input}
-                `}
-                type="text"
-                name="username"
-                value={username}
-                onChange={evt => setUsername(evt.target.value)}
-                placeholder="Username"
-                autoFocus
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              <input
-                css={css`
-                  ${input}
-                `}
-                type="password"
-                name="password"
-                value={password}
-                onChange={evt => setPassword(evt.target.value)}
-                placeholder="Password"
-              />
-            </label>
-          </div>
-          <div>
-            <small
-              css={css`
-                color: #1a73e8;
-              `}
-            >
-              Forgot Password?
-            </small>
-          </div>
-          <div>
-            <button
-              css={css`
-                ${btn}
-              `}
-              type="submit"
-            >
-              Login
-            </button>
-          </div>
-        </form>
       </div>
-    </div>
-  );
+    );
+  }
 
   function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
     evt.preventDefault();
