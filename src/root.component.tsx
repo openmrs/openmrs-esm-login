cd1ef866181173b619cad74aa36733fbe41c72df
Brandon Istenes <bistenes@gmail.com>
2020-04-08 10:01:45 -0700

    Add descriptions to config schema and update config-cli

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import openmrsRootDecorator from "@openmrs/react-root-decorator";
-import { defineConfigSchema } from "@openmrs/esm-module-config";
+import { defineConfigSchema, validators } from "@openmrs/esm-module-config";
 import { createErrorHandler } from "@openmrs/esm-error-handling";
 import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./login/login.component";
@@ -11,10 +11,16 @@ defineConfigSchema("@openmrs/esm-login", {
   chooseLocation: {
     enabled: {
       default: true,
+      description:
+        "Whether to show a 'Choose Location' screen after login. " +
+        "If true, the user will be taken to the loginSuccess URL after they " +
+        "choose a location.",
+      validators: [validators.isBoolean],
     },
   },
   links: {
     loginSuccess: {
+      description: "Where to take the user after they are logged in.",
       url: {
         default: "/home",
       },
@@ -25,10 +31,13 @@ defineConfigSchema("@openmrs/esm-login", {
   },
   logo: {
     src: {
-      default: null, // defaults to an SVG Sprite
+      default: null,
+      description:
+        "A path or URL to an image. Defaults to the OpenMRS SVG sprite.",
     },
     alt: {
       default: "Logo",
+      description: "Alt text, shown on hover",
     },
   },
 });
