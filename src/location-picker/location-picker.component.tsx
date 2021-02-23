7e35abd17d53c2f56d0695f067435e4dd2e87a2f
Donald Kibet <chelashawdonald@yahoo.com>
2021-02-23 09:55:39 +0300

    MF-471 Location Selector: Complete carbonization

--- src/location-picker/location-picker.component.tsx
+++ src/location-picker/location-picker.component.tsx
@@ -1,50 +1,17 @@
 import React from "react";
-import { always } from "kremling";
 import { debounce, isEmpty } from "lodash";
 import { createErrorHandler } from "@openmrs/esm-error-handling";
-import { Trans } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import { LocationEntry } from "../types";
 import styles from "./location-picker.component.scss";
-
-const CardHeader: React.FC = (props) => (
-  <div className={styles["card-header"]}>
-    <h3 className={`omrs-margin-8 omrs-margin-left-12`}>{props.children}</h3>
-  </div>
-);
-
-interface RadioInputProps {
-  option: LocationEntry;
-  current: string;
-  changeLocationData: (data: Partial<LocationDataState>) => void;
-}
-
-const RadioInput: React.FC<RadioInputProps> = ({
-  option,
-  current,
-  changeLocationData,
-}) => (
-  <div className="omrs-radio-button">
-    <input
-      id={option.resource.id}
-      type="radio"
-      name="location"
-      value={option.resource.id}
-      checked={current === option.resource.id}
-      onChange={(evt) =>
-        changeLocationData({ activeLocation: evt.target.value })
-      }
-    />
-    <label htmlFor={option.resource.id} className="omrs-padding-4">
-      {option.resource.name}
-    </label>
-  </div>
-);
-
+import Search from "carbon-components-react/es/components/Search";
+import RadioButtonGroup from "carbon-components-react/es/components/RadioButtonGroup";
+import RadioButton from "carbon-components-react/es/components/RadioButton";
+import Button from "carbon-components-react/es/components/Button";
 interface LocationDataState {
   activeLocation: string;
   locationResult: Array<LocationEntry>;
 }
-
 interface LocationPickerProps {
   currentUser: string;
   loginLocations: Array<LocationEntry>;
@@ -62,6 +29,8 @@ const LocationPicker: React.FC<LocationPickerProps> = ({
   hideWelcomeMessage,
   currentLocationUuid,
 }) => {
+  const pageSize = 8;
+  const { t } = useTranslation();
   const userDefaultLoginLocation: string = "userDefaultLoginLocationKey";
   const getDefaultUserLoginLocation = (): string => {
     const userLocation = window.localStorage.getItem(
@@ -81,11 +50,6 @@ const LocationPicker: React.FC<LocationPickerProps> = ({
   const inputRef = React.useRef();
 
   const searchTimeout = 300;
-
-  const autoFocusSearchInput = React.useCallback(
-    (el: HTMLInputElement) => (el ? el.focus() : null),
-    []
-  );
 
   React.useEffect(() => {
     if (isSubmitting) {
@@ -183,65 +147,84 @@ const LocationPicker: React.FC<LocationPickerProps> = ({
   };
 
   return (
-    <div className={`canvas ${styles["locationPickerContainer"]}`}>
-      {!hideWelcomeMessage && (
-        <h2 className={styles["welcome-msg"]}>
-          <Trans i18nKey="welcome">Welcome</Trans> {currentUser}
-        </h2>
-      )}
+    <div className={styles["locationPickerContainer"]}>
       <form onSubmit={handleSubmit}>
-        <div className={`${styles["location-card"]} omrs-card`}>
-          {!hideWelcomeMessage && (
-            <CardHeader>
-              <Trans i18nKey="location">Location</Trans>
-            </CardHeader>
-          )}
-          <div className="omrs-input-group omrs-padding-12">
-            <input
-              className="omrs-input-underlined"
-              placeholder="Search for location"
-              aria-label="Search for location"
+        <div className={`${styles["location-card"]}`}>
+          <div className={styles["welcomeContainer"]}>
+            <p className={styles["welcomeTitle"]}>
+              {t("welcome", "Welcome")} {currentUser}
+            </p>
+            <p className={styles["welcomeMessage"]}>
+              {t(
+                "selectYourLocation",
+                "Select your location from the list below. Use the search bar to search for your location."
+              )}
+            </p>
+          </div>
+          <div>
+            <Search
+              labelText="Search for location"
+              id="search-1"
+              placeHolderText={t("searchForLocation", "Search for a location")}
               onChange={(ev) => search(ev.target.value)}
-              ref={autoFocusSearchInput}
+              autoFocus={true}
+              name="searchForLocation"
             />
           </div>
-          {!isEmpty(locationData.locationResult) && (
-            <div className={styles["location-radio-group"]}>
-              {locationData.locationResult.map((entry) => (
-                <RadioInput
-                  key={entry.resource.id}
-                  current={locationData.activeLocation}
-                  option={entry}
-                  changeLocationData={changeLocationData}
-                />
-              ))}
-            </div>
-          )}
-          {locationData.locationResult.length === 0 && (
-            <p className="omrs-type-body-regular omrs-padding-8">
-              <Trans i18nKey="locationNotFound">
-                Sorry, no location has been found
-              </Trans>
+          <div className={styles["searchResults"]}>
+            <p>
+              {searchTerm
+                ? `${locationData.locationResult.length} ${
+                    locationData.locationResult.length > 1
+                      ? t("matches", "matches")
+                      : t("match", "match")
+                  } ${t("found", "found")}`
+                : `${t("showing", "Showing")} ${pageSize} ${t("of", "of")} ${
+                    locationData.locationResult.length
+                  } ${t("locations", "locations")}`}
             </p>
-          )}
-          <div className={styles["center"]}>
-            <p className={styles["error-msg"]} />
           </div>
-        </div>
-        <div className={styles["center"]}>
-          <button
-            className={always(
-              `omrs-margin-16 omrs-btn omrs-rounded ${styles["location-submit-btn"]}`
-            ).toggle(
-              "omrs-filled-disabled",
-              "omrs-filled-action",
-              !locationData.activeLocation
+          <div className={styles["locationResultsContainer"]}>
+            {!isEmpty(locationData.locationResult) && (
+              <RadioButtonGroup
+                valueSelected={locationData.activeLocation}
+                orientation="vertical"
+                name={locationData.activeLocation}
+                onChange={(ev) => {
+                  changeLocationData({ activeLocation: ev.toString() });
+                }}
+              >
+                {locationData.locationResult.slice(0, pageSize).map((entry) => (
+                  <RadioButton
+                    className={styles["locationRadioButton"]}
+                    key={entry.resource.id}
+                    id={entry.resource.name}
+                    labelText={entry.resource.name}
+                    value={entry.resource.id}
+                  />
+                ))}
+              </RadioButtonGroup>
             )}
-            type="submit"
-            disabled={!locationData.activeLocation}
-          >
-            <Trans i18nKey="confirm">Confirm</Trans>
-          </button>
+            {locationData.locationResult.length === 0 && (
+              <p className={styles["locationNotFound"]}>
+                <Trans i18nKey="locationNotFound">
+                  Sorry, no location has been found
+                </Trans>
+              </p>
+            )}
+            <div className={styles["center"]}>
+              <p className={styles["error-msg"]} />
+            </div>
+          </div>
+          <div className={styles["confirmButton"]}>
+            <Button
+              kind="primary"
+              type="submit"
+              disabled={!locationData.activeLocation}
+            >
+              <Trans i18nKey="confirm">Confirm</Trans>
+            </Button>
+          </div>
         </div>
       </form>
     </div>
